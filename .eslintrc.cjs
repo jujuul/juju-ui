29966d7520d0fb239d9e63e0f487d06930ee8394
jujuul <576583430@qq.com>
2022-08-21 22:44:28 +0800

    feat: add eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    // "plugin:vue/vue3-essential",
+  ],
+  overrides: [],
+  // "parser": "@typescript-eslint/parser",
+  // "parser": "vue-eslint-parser",
+  // "parser": "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+  },
+}
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import Test from './components/Test';
-</script>
-
 <template>
   <div>
     <Test />
   </div>
 </template>
+<script setup lang="ts">
+import Test from "./components/Test";
+</script>
